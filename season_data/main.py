bee79e12a0d1e644ba41b3fbb2c1a5f0495c85ed
Carolyn Hanrahan <carolyn.hanrahan23@gmail.com>
2024-03-20 14:39:37 -0400

    add test file with season data

--- season_data/main.py
+++ season_data/main.py
@@ -22,7 +22,7 @@ df_selection = pd.read_csv('CoyoteMetabarcodingSelections_20231205.csv')
 LabID = df_selection["LabID"].tolist()
 Year = df_selection["YearCollected"].tolist()
 
-print(len(LabID), len(Year))
+# print(len(LabID), len(Year))
 
 # Populate a dictionary with LabID and Year lists
 if len(LabID) == len(Year):
@@ -42,9 +42,35 @@ df_results = pd.read_csv('Metabarcoding_clean_results.csv')
 
 # for i in df_results.index:
 
-print(df_results.keys())
+#print(df_results.keys())
 
 # we have now extracted the keys from the results dataframe. Next, we pass
 # these values into the dictonary to create a list of seasons. Then, append list to existing 
 # df_results dataframe and save to csv. 
 
+seasons = []
+
+for i, key in enumerate(df_results.keys()):
+    if "S2" in key:
+
+        key = key.replace("_", "-")
+        
+        if key in season_samples.keys():
+            seasons.append(season_samples[key])
+        else:
+            seasons.append('')
+    else:
+        seasons.append("")
+
+# print(seasons)
+
+df_seasons = pd.DataFrame(seasons)
+
+print(len(df_results.columns[0]))
+print(df_results.shape[0])
+
+df_concat = pd.concat([df_results, df_seasons], axis=0, ignore_index=True) 
+
+df_concat.to_csv('test.csv', index=False)
+
+# df_seasons.to_csv('Seasons.csv', index=False)
